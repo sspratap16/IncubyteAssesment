bb233f8ecb52eba23ad25f83df7b52e1b6c2894c
sspratap16 <37359057+sspratap16@users.noreply.github.com>
2025-04-04 19:27:58 +0530

    Step 4 - Handle newlines as delimiters

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,7 +1,9 @@
 class StringCalculator
+  DEFAULT_DELIMITERS = /[,\n]/
+
   def self.add(numbers_string)
     return 0 if numbers_string.empty?
-    numbers = numbers_string.split(',').map(&:to_i)
+    numbers = numbers_string.split(DEFAULT_DELIMITERS).map(&:to_i)
     numbers.sum
   end
 end
--- test/services/string_calculator_test.rb
+++ test/services/string_calculator_test.rb
@@ -14,4 +14,17 @@ class StringCalculatorTest < ActiveSupport::TestCase
     assert_equal 6, StringCalculator.add("1,5")
     assert_equal 30, StringCalculator.add("10,20")
   end
+
+  def test_add_returns_sum_for_multiple_comma_separated_numbers
+    assert_equal 15, StringCalculator.add("1,2,3,4,5")
+  end
+
+  def test_add_handles_newlines_as_delimiters
+    assert_equal 6, StringCalculator.add("1\n2,3")
+    assert_equal 6, StringCalculator.add("1,2\n3")
+  end
+  
+  def test_add_handles_consecutive_delimiters_resulting_in_zero
+     assert_equal 3, StringCalculator.add("1,\n2")
+  end
 end
