3cd7c116cf3f6e82dd4d47c577306a0078e077c4
sspratap16 <37359057+sspratap16@users.noreply.github.com>
2025-04-04 19:36:33 +0530

    Step 5 - Handle custom delimiters and negative numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -3,7 +3,35 @@ class StringCalculator
 
   def self.add(numbers_string)
     return 0 if numbers_string.empty?
-    numbers = numbers_string.split(DEFAULT_DELIMITERS).map(&:to_i)
+
+    delimiter, string_to_process = extract_delimiter_and_numbers(numbers_string)
+    return 0 if string_to_process.empty?
+
+    numbers = parse_numbers(string_to_process, delimiter)
+    negatives = numbers.select { |n| n < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
     numbers.sum
+  end
+
+  private
+
+  def self.extract_delimiter_and_numbers(input_string)
+    if input_string.start_with?("//")
+      parts = input_string.split("\n", 2)
+      delimiter_definition = parts[0]
+      custom_delimiter = delimiter_definition[2..-1]
+      delimiter = Regexp.new(Regexp.escape(custom_delimiter))
+      numbers_part = parts[1] || ""
+      [delimiter, numbers_part]
+    else
+      [DEFAULT_DELIMITERS, input_string]
+    end
+  end
+
+  def self.parse_numbers(numbers_part, delimiter)
+    return [] if numbers_part.nil? || numbers_part.empty?
+    numbers_part.split(delimiter).map(&:to_i)
   end
 end
--- test/services/string_calculator_test.rb
+++ test/services/string_calculator_test.rb
@@ -27,4 +27,47 @@ class StringCalculatorTest < ActiveSupport::TestCase
   def test_add_handles_consecutive_delimiters_resulting_in_zero
      assert_equal 3, StringCalculator.add("1,\n2")
   end
+
+  def test_add_supports_single_custom_delimiter
+    assert_equal 3, StringCalculator.add("//;\n1;2")
+  end
+  
+  def test_add_supports_different_custom_delimiter
+    assert_equal 6, StringCalculator.add("//*\n1*2*3")
+  end
+  
+  def test_add_handles_custom_delimiter_definition_with_no_numbers
+    assert_equal 0, StringCalculator.add("//;\n")
+  end
+  
+   def test_add_handles_custom_delimiter_with_one_number
+     assert_equal 5, StringCalculator.add("//;\n5")
+   end
+  
+  def test_add_correctly_handles_regex_special_character_delimiter
+    assert_equal 6, StringCalculator.add("//.\n1.2.3")
+  end
+
+  def test_add_throws_exception_for_single_negative_number
+    exception = assert_raises(RuntimeError) do
+      StringCalculator.add("1,-2,3")
+    end
+    assert_equal "negative numbers not allowed -2", exception.message
+  end
+  
+  def test_add_throws_exception_listing_all_negative_numbers
+    exception = assert_raises(RuntimeError) do
+      StringCalculator.add("//;\n1;-2;3;-4;-5")
+    end
+    assert_equal "negative numbers not allowed -2,-4,-5", exception.message
+  end
+  
+  def test_add_does_not_throw_exception_for_zero_or_positives
+    assert_nothing_raised do
+       StringCalculator.add("1,0,2")
+       StringCalculator.add("//;\n1;0;2")
+    end
+    assert_equal 3, StringCalculator.add("1,0,2")
+    assert_equal 3, StringCalculator.add("//;\n1;0;2")
+  end
 end
